b5477b2ec6821c5418e08d202f82a7982c8e4654
IlmanNawali <79490380+IlmanNawali@users.noreply.github.com>
2022-06-10 20:39:55 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ module.exports = {
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: {DB_NAME},
+    database: `${DB_NAME}_development`,
     host: DB_HOST,
     port: DB_PORT,
     dialect: "postgres"
@@ -18,7 +18,7 @@ module.exports = {
   test: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: {DB_NAME},
+    database: `${DB_NAME}_test`,
     host: DB_HOST,
     port: DB_PORT,
     dialect: "postgres"
@@ -26,9 +26,10 @@ module.exports = {
   production: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: {DB_NAME},
+    database: `${DB_NAME}_production`,
     host: DB_HOST,
     port: DB_PORT,
     dialect: "postgres"
   }
+
 }

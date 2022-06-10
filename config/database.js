a2bfe7110c45c9029dd629c722bc1cfe9e48bef4
IlmanNawali <79490380+IlmanNawali@users.noreply.github.com>
2022-06-10 21:03:15 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
-const DB_USER = prosecc.env.DB_USER || "hgqkizfmpgcnbq";
-const DB_PASSWORD = prosecc.env.DB_PASSWORD || "0a362bdf5906ee4ba528a271979e36c44e6c647f13693a470ee49e128829dba1";
-const DB_NAME = prosecc.env.DB_NAME || "d6bh8j0a3c56vg";
-const DB_HOST = prosecc.env.DB_HOST || "ec2-3-226-163-72.compute-1.amazonaws.com";
-const DB_PORT = prosecc.env.DB_PORT || "5432";
+const DB_USER = process.env.DB_USER || "hgqkizfmpgcnbq";
+const DB_PASSWORD = process.env.DB_PASSWORD || "0a362bdf5906ee4ba528a271979e36c44e6c647f13693a470ee49e128829dba1";
+const DB_NAME = process.env.DB_NAME || "d6bh8j0a3c56vg";
+const DB_HOST = process.env.DB_HOST || "ec2-3-226-163-72.compute-1.amazonaws.com";
+const DB_PORT = process.env.DB_PORT || "5432";
 
 
 

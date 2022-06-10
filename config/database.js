a13205ba6fd8289c60c8ae4dddd1ff3aa4ebf68a
IlmanNawali <79490380+IlmanNawali@users.noreply.github.com>
2022-06-10 21:00:35 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -6,6 +6,14 @@ const {
   DB_PORT = "5432",
 } = process.env;
 
+const DB_USER = prosecc.env.DB_USER || "hgqkizfmpgcnbq";
+const DB_PASSWORD = prosecc.env.DB_PASSWORD || "0a362bdf5906ee4ba528a271979e36c44e6c647f13693a470ee49e128829dba1";
+const DB_NAME = prosecc.env.DB_NAME || "d6bh8j0a3c56vg";
+const DB_HOST = prosecc.env.DB_HOST || "ec2-3-226-163-72.compute-1.amazonaws.com";
+const DB_PORT = prosecc.env.DB_PORT || "5432";
+
+
+
 module.exports = {
   development: {
     username: DB_USER,
@@ -24,13 +32,17 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    username: "hgqkizfmpgcnbq",
-    password: "0a362bdf5906ee4ba528a271979e36c44e6c647f13693a470ee49e128829dba1",
-    database: "d6bh8j0a3c56vg",
-    host: "ec2-3-226-163-72.compute-1.amazonaws.com",
-    port: "5432",
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}`,
+    host: DB_HOST,
+    port: DB_PORT,
     dialect: "postgres",
-  },
-
-
+    dialectOptions: {
+      ssl:{
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 }
